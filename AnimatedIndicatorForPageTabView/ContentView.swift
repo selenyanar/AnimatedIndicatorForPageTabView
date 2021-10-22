7f9811cdf08707b711a5fafc99571983d5d04c20
Selen <selenyanar@gmail.com>
2021-10-22 20:56:56 +0300

    Initialize TabView with scroll animation.

--- AnimatedIndicatorForPageTabView/ContentView.swift
+++ AnimatedIndicatorForPageTabView/ContentView.swift
@@ -9,13 +9,102 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Home()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Home()
+    }
+}
+
+
+struct Home: View {
+    
+    //Colors as tabs...
+    //Use your own tabs here...
+    var colors : [Color] = [.red, .blue, .pink, .purple]
+    
+    // Offset...
+    @State var offset: CGFloat = 0
+    
+    var body: some View {
+        
+        //Tabview has problem in ignoring top view. Fix by using scroll view...
+        
+        ScrollView(.init()) {
+            
+            TabView {
+                
+                ForEach(colors.indices, id: \.self) {index in
+                    
+                    if index == 0 {
+                        colors[index]
+                            .overlay(
+                                //Geometry reader for getting offset...
+                                GeometryReader {proxy -> Color in
+                                    
+                                    let minX = proxy.frame(in: .global).minX
+                                    
+                                    
+                                    DispatchQueue.main.async {
+                                        
+                                        withAnimation(.default) {
+                                            
+                                            self.offset = -minX
+                                            
+                                        }
+                                    }
+                                    
+                                    return Color.clear
+                                }
+                                .frame(width: 0, height: 0)
+                                , alignment: .leading
+                            )
+                        
+                    }
+                    else {
+                        colors[index]
+                    }
+                }
+            }
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+            .overlay(
+            
+                //Animated indicators...
+                HStack(spacing: 15) {
+                   
+                    ForEach(colors.indices, id: \.self) {index in
+                        
+                        Capsule()
+                            .fill(Color.white)
+                            .frame(width: getIndex() == index ? 20 : 7, height: 7)
+                        
+                    }
+                }
+                .padding(.bottom, UIApplication.shared.windows.first?.safeAreaInsets.bottom)
+                .padding(.bottom, 10)
+                , alignment: .bottom
+            )
+        }
+        .ignoresSafeArea()
+    }
+    //Getting index...
+    func getIndex() -> Int {
+        
+        let index = Int(round(Double(offset / getWidth())))
+        
+        return index
+    }
+}
+
+//Extending view to get width...
+extension View {
+    
+    func getWidth() -> CGFloat {
+        
+        return UIScreen.main.bounds.width
+        
     }
 }
