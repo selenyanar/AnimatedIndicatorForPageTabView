e13d6f1b0b5d6af85ef5b264ccff0bff97b5bf1c
Selen <selenyanar@gmail.com>
2021-10-22 20:00:45 +0300

    Initial Commit

---
+++ AnimatedIndicatorForPageTabView/AnimatedIndicatorForPageTabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedIndicatorForPageTabViewApp.swift
+//  AnimatedIndicatorForPageTabView
+//
+//  Created by Selen Yanar on 22.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedIndicatorForPageTabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
